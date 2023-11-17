75d558604a5dbd650919da570600133ec7b41c25
André Pestana Silva <atlas.ifpa@gmail.com>
2023-11-17 12:24:57 -0300

    core: iniciando refatoração para react

--- backend/routes/products.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import * as productController from '../controller/product.controller.js'
-
-const productRouter = express.Router();
-
-productRouter.use('/register', productController.register);
-
-export default productRouter;
--- backend/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import * as userController from '../controller/user.controller.js';
-
-const userRouter = express.Router();
-
-userRouter.post('/login', userController.login);
-
-export default userRouter;
